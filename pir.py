23b809cf489fe21eb6d9b5b2acd2ae4d38593a35
Slyke <steven.lawler777@gmail.com>
2020-01-23 15:18:43 -0800

    Added motion led GPIO Updated URLs

--- pir.py
+++ pir.py
@@ -3,9 +3,10 @@ import datetime
 import os
 import requests
 import sys
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor, LED
 
 pirGpioPort = int(os.environ.get('GPIO_PORT', 17));
+pirGpioPortLed = int(os.environ.get('GPIO_PORT_LED', 26));
 httpUrl = os.environ.get('HTTP_URL', '');
 httpAuthUsername = os.environ.get('HTTP_AUTH_USERNAME', "");
 httpAuthPassword = os.environ.get('HTTP_AUTH_PASSWORD', "");
@@ -18,11 +19,14 @@ if (httpUrl == ''):
   exit(1);
 
 pir = MotionSensor(pirGpioPort);
+motionLed = LED(pirGpioPortLed);
+
 print("[{} - {}]: Sending Requests To: {}".format(datetime.datetime.now(), time.time(), httpUrl))
 print("[{} - {}]: Auth: {}".format(datetime.datetime.now(), time.time(), (not(httpAuthUsername == "") and not(httpAuthPassword == ""))))
 print("[{} - {}]: httpDataInUrl: {}".format(datetime.datetime.now(), time.time(), httpDataInUrl))
 print("[{} - {}]: Request Method: {}".format(datetime.datetime.now(), time.time(), httpMethod))
 print("[{} - {}]: GPIO: {}".format(datetime.datetime.now(), time.time(), pirGpioPort))
+print("[{} - {}]: LED: {}".format(datetime.datetime.now(), time.time(), pirGpioPortLed))
 print("[{} - {}]: StationId: {}".format(datetime.datetime.now(), time.time(), stationId))
 print("")
 print("[{} - {}]: Motion Detector Activated".format(datetime.datetime.now(), time.time()))
@@ -44,12 +48,14 @@ try:
       motionDetected = True;
       print("[{} - {}]: Event: 'Motion detected', ".format(datetime.datetime.now(), time.time()), end = " ");
       sendRequest(True);
+      motionLed.on();
       time.sleep(0.5);
     elif (not pir.motion_detected and motionDetected):
       motionDetected = False
       # pir.wait_for_no_motion();
       print("[{} - {}]: Event: 'Motion detection end', ".format(datetime.datetime.now(), time.time()), end = " ");
       sendRequest(False);
+      motionLed.off();
 
     sys.stdout.flush();
 
